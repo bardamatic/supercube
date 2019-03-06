7d5507c7b31d110bdab5f405917bec249dceb472
bardamatic <bardamatic@gmail.com>
2019-03-06 13:58:24 +0200

    Update shims-vue.d.ts

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,7 +1,4 @@
-declare module 'worker-loader!*' {
-    class WebpackWorker extends Worker {
-        constructor();
-    }
-
-    export default WebpackWorker
+declare module '*.vue' {
+    import Vue from 'vue'
+    export default Vue
 }
