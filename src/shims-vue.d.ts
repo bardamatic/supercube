802bcb696807786d517aa81b8134c241d2980b5f
bardamatic <bardamatic@gmail.com>
2019-03-06 13:49:37 +0200

    commits

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,4 +1,7 @@
-declare module '*.vue' {
-    import Vue from 'vue';
-    export default Vue;
+declare module 'worker-loader!*' {
+    class WebpackWorker extends Worker {
+        constructor();
+    }
+
+    export default WebpackWorker
 }
