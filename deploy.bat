3552b7c78c1d72f6fbd0aeacf3b26533dacfd7cb
bardamatic <bardamatic@gmail.com>
2019-03-06 13:43:01 +0200

    Update deploy.bat

--- deploy.bat
+++ deploy.bat
@@ -4,7 +4,7 @@ git init
 git add -A
 git commit -m "deploy"
 
-git remote add origin https://github.com/avaver/avaver.github.io.git
+git remote add origin https://github.com/bardamatic/bardamatic.github.io.git
 git push -f -u origin master
 
 cd ..
